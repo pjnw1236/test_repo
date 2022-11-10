4186ed05bf6d3ee78eca93f18f3a3232ec3a017a
pjnw1236 <pjnw1236@gmail.com>
2022-11-10 11:17:27 +0900

    configure .gitignore

---
+++ yes27/yes27/src/main/java/com/yes27/Yes27Application.java
@@ -0,0 +1,13 @@
+package com.yes27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yes27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yes27Application.class, args);
+	}
+
+}
